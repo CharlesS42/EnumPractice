1dedf5303ec978bae9ecb746f87a5b267fda042a
CharlesS42 <charles.seg42@gmail.com>
2023-09-29 10:52:35 -0400

    Add project files.

---
+++ EnumPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFile file1 = new MyFile("picture.txt", @"C:\users\cseguin\" /* @ signifies no escape character */
+                , Rights.READABLE | Rights.WRITABLE, null);
+
+            // how do we know which permissions were set on file 1?
+
+                //what happens if we target the return of the enum in general
+                Console.WriteLine(file1.FileRights);
+                // casting
+                Console.WriteLine((Rights)file1.FileRights);
+                Console.WriteLine((int)file1.FileRights);
+
+            // how do we add to an entity's enum
+
+            file1.FileRights |= Rights.EXECUTABLE;
+            Console.WriteLine(file1.FileRights);
+        }
+    }
+}
